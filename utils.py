ba17f80a6a05d45b944ff960aaeb30c90b92e9d2
Shyam Patadia <shyampatadia22@gmail.com>
2022-05-02 13:20:01 +0530

    Adding the utils.py and wiki_extractor.py files

---
+++ utils.py
@@ -0,0 +1,59 @@
+import requests
+from bs4 import BeautifulSoup
+import wikipediaapi
+from tqdm import tqdm
+import json
+
+#constants
+WIKIPEDIA_URL_EXTENSION = "https://en.wikipedia.org"
+
+# initializing wikipediaapi class
+wikipedia_api_class = wikipediaapi.Wikipedia(
+    language='en', extract_format=wikipediaapi.ExtractFormat.WIKI)
+
+
+def generate_output_dictionary(element):
+    """Generates a dictionary object containing a URL of the related article and its content
+
+    Args:
+        a HTML element containing from which we want to extract the URL
+
+    Returns:
+        _dictionary_: contains 2 keys named "url" and "content"
+    """
+    if element.has_attr('data-serp-pos') == True:
+        ouput_dict = {}
+        ouput_dict['url'] = WIKIPEDIA_URL_EXTENSION + element['href'] #extracting article URL
+        ouput_dict['content'] = wikipedia_api_class.page(element['title']).text #using wikipedia_api library to extract the content from the article 
+        return ouput_dict
+
+def generate_final_list(page_url):
+    """Send a Get request to the URL and get the reponse back and then extract the required information from the response.
+
+    Args:
+        page_url (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    response = requests.get(url=page_url) #sending a get request
+    soup = BeautifulSoup(response.content, 'html.parser') #Parsing the response received in the request that we made
+    link_info = soup.find('div', {"id": "mw-content-text"}).find('ul',
+                                                                class_="mw-search-results").find_all('a') #getting all the elements required to extract information and stroring them in a list
+    list_final_output = list(tqdm(map(generate_output_dictionary, link_info))) #using generate_output_dictionary function to generate content and extract url
+    return list_final_output
+
+
+def generate_json(final_list, output_file_name): 
+    """This fucntions will generate a json file given a certain input
+
+    Args:
+        final_list (list): a list object that we want to store it in json format
+        output_file_name (string): name of the output file
+    """
+    if output_file_name.contains(".json"):
+        with open(output_file_name, 'w') as f:
+            json.dump(final_list, f) 
+    else:
+        with open(output_file_name+".json", 'w') as f:
+            json.dump(final_list, f)  
---
+++ wiki_extractor.py
@@ -0,0 +1,38 @@
+import argparse
+import sys
+
+from utils import *
+
+# initializing the arg_parser which will take the input from the command line
+parser = argparse.ArgumentParser()
+parser.add_argument("--keyword", type=str,
+                    help="Enter the Keyword you want to search on Wikipedia")
+parser.add_argument("--num_urls", type=int,
+                    help="Enter the number of urls that you want to scrape")
+parser.add_argument("--output", type=str,
+                    help="Please input the filename in which you want the output to be in, it should be json!")
+args = parser.parse_args()
+
+# formatting input data according to api call format
+args.keyword = str(args.keyword).replace(" ", "+")
+
+#constants
+WIKIPEDIA_API = f"https://en.wikipedia.org/w/index.php?title=Special:Search&limit={0}&offset={1}&profile=default&search={2}&ns0=1"
+OFF_SET = 0
+
+
+if (args.num_urls > 10000):
+    sys.exit("An error has occurred while searching: Could not retrieve results. Up to 10000 search results are supported, but results starting at 10000 were requested.")
+elif (args.num_urls <= 5000):
+    page_url = WIKIPEDIA_API.format(args.num_urls, OFF_SET, args.keyword)
+    listOfContent = generate_final_list(page_url)
+    generate_json(listOfContent, args.output)
+else:
+    page_url = WIKIPEDIA_API.format(args.num_urls, OFF_SET, args.keyword)
+    listPart1 = generate_final_list(page_url)
+    OFF_SET = 5000
+    rmaining_number_of_urls = args.num_urls - OFF_SET
+    page_url_offset_changed = WIKIPEDIA_API.format(rmaining_number_of_urls, OFF_SET, args.keyword)
+    listPart2 = generate_final_list(page_url_offset_changed)
+    finalList = listPart1 + listPart2 #concatenating both the list
+    generate_json(finalList, args.output)
